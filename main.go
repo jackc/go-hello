77f5be5e67ef425be5e135992a4c755e6d7cab76
Jack Christensen <jack@jackchristensen.com>
2014-05-04 22:06:28 -0500

    Add version option

--- main.go
+++ main.go
@@ -6,9 +6,12 @@ import (
 	"os"
 )
 
+const VERSION = "0.2.0pre"
+
 func main() {
 	var opts struct {
-		Name string `short:"n" long:"name" description:"Name to greet" default:"world"`
+		Name    string `short:"n" long:"name" description:"Name to greet" default:"world"`
+		Version bool   `long:"version" description:"Display version and exit"`
 	}
 
 	parser := flags.NewParser(&opts, flags.Default)
@@ -16,6 +19,11 @@ func main() {
 	if _, err := parser.Parse(); err != nil {
 		fmt.Fprintln(os.Stderr, "Error parsing command line")
 		os.Exit(1)
+	}
+
+	if opts.Version {
+		fmt.Println("go-hello " + VERSION)
+		os.Exit(0)
 	}
 
 	fmt.Printf("Hello, %s!\n", opts.Name)
