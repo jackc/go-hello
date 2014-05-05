e557bbc61fca084ebbb4d948000e2394047244b8
Jack Christensen <jack@jackchristensen.com>
2014-05-04 22:04:47 -0500

    Add command option for name

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/jessevdk/go-flags"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	var opts struct {
+		Name string `short:"n" long:"name" description:"Name to greet" default:"world"`
+	}
+
+	parser := flags.NewParser(&opts, flags.Default)
+	parser.Usage = "[options]"
+	if _, err := parser.Parse(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error parsing command line")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Hello, %s!\n", opts.Name)
 }
